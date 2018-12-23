1a1e507cb146dc45ec136137ff0ba7e47ffde023
unknown <anetzerel@yotpo.com>
2018-12-23 12:11:58 +0200

    initial commit

---
+++ responses.js
@@ -0,0 +1,32 @@
+var soundPathBase = "sounds/";
+var itayResponses = {
+	"tasty":[{
+		"text":"לא טעים",
+		"soundSrc": soundPathBase + "lo-taim.m4a"
+	},
+	{
+		"text":"מאוד טעים, אבל אני לא חוזר לשם",
+		"soundSrc": soundPathBase + "lo-taim.m4a"
+	},
+	{
+		"text":" to an extent טעים",
+		"soundSrc": soundPathBase + "lo-taim.m4a"
+	},
+	{
+		"text":"טעים",
+		"soundSrc": soundPathBase + "lo-taim.m4a"
+	}],
+	"notTasty": [
+	{
+		"text":"לא טעים",
+		"soundSrc": soundPathBase + "lo-taim.m4a"
+	},
+	{
+		"text":"טעים, אבל לא טעים",
+		"soundSrc": soundPathBase + "lo-taim.m4a"
+	},
+	{
+		"text":"לא טעים",
+		"soundSrc": soundPathBase + "lo-taim.m4a"
+	}]
+}
